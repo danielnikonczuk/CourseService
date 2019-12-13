ae40c306c56ba1f208dd1b02f9669b6eb9a5d7ca
Daniel Nikończuk <daniel.nikonczuk@gmail.com>
2019-12-13 08:45:03 +0100

    Initial commit

---
+++ Models/Models.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CourseService.Models
+{
+    public class CourseServiceContext : DbContext
+    {      
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public CourseServiceContext(DbContextOptions<CourseServiceContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
