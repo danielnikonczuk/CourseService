f821ba522d94bdb7857a7efaba6a9049890988d3
Daniel Nikończuk <34033876+danielnikonczuk@users.noreply.github.com>
2019-12-18 12:49:49 +0100

    no message

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using CourseService.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,27 +11,28 @@ namespace CourseService
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .Build();
+            Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine(Configuration.GetValue<string>("CONNECTION_STRING"));
+
             services.AddDbContext<CourseServiceContext>(options =>
-                options.UseSqlite(Configuration.GetValue<string>("CONNECTION_STRING")));
+                options.UseSqlServer(Configuration.GetValue<string>("CONNECTION_STRING")));
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,6 +48,16 @@ namespace CourseService
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+
+            using var context = serviceScope.ServiceProvider.GetService<CourseServiceContext>();
+            context.Database.Migrate();
         }
     }
 }
