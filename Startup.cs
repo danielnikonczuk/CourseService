9ffc3508e384b2937f54cc6645e97a052c400d86
Daniel Nikończuk <34033876+danielnikonczuk@users.noreply.github.com>
2019-12-18 13:47:24 +0100

    no message

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,3 @@
-using System;
 using CourseService.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,18 +20,14 @@ namespace CourseService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Console.WriteLine(Configuration.GetValue<string>("CONNECTION_STRING"));
-
             services.AddDbContext<CourseServiceContext>(options =>
                 options.UseSqlServer(Configuration.GetValue<string>("CONNECTION_STRING")));
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to cosqlsenfigure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            UpdateDatabase(app);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -48,16 +43,6 @@ namespace CourseService
             {
                 endpoints.MapControllers();
             });
-        }
-
-        private static void UpdateDatabase(IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices
-                .GetRequiredService<IServiceScopeFactory>()
-                .CreateScope();
-
-            using var context = serviceScope.ServiceProvider.GetService<CourseServiceContext>();
-            context.Database.Migrate();
         }
     }
 }
