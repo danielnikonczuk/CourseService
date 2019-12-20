cacc883e91492bc7ab01be8c236c3edb0ece318f
Daniel Nikończuk <daniel.nikonczuk@gmail.com>
2019-12-20 11:03:06 +0100

    Adding integration tests

--- CourseService.Integration.Test/HealthCheckTest.cs
+++ CourseService.Integration.Test/HealthCheckTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,15 +8,20 @@ namespace CourseService.Integration.Test
     [Trait("Category", "Integration")]
     public class HealthCheckTest
     {
-        private HttpClient httpClient;
+        private HttpClient _httpClient;
 
         public HealthCheckTest()
         {
+            _httpClient = new HttpClient();
         }
 
         [Fact]
         public async Task HealthCheckTest_ReturnsHealthyStatus()
         {
+            using var response = await _httpClient.GetAsync("/health");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(await response.Content.ReadAsStringAsync(), "Healthy");
 
         }
     }
